de2b1c9df0e47e83b5c99767a8da14fdb91cca3e
angelesbg823 <angelesbarria823@gmail.com>
2022-04-05 00:58:30 -0400

    finished: validar_edad.rb

---
+++ pares.rb
@@ -0,0 +1,5 @@
+# Crear el programa pares.rb donde se sumen únicamente los números pares dentro del ciclo
+# entre 0 y un número ingresado por el usuario al momento de cargar el programa.
+# Uso:
+# pares.rb 100
+# 2550
